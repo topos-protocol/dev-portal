ff9a49b36d94a169dfd3851dde17a3e2a3a053f7
Thomas Lanciaux <thomaslanciaux1@gmail.com>
2023-09-27 15:47:11 +0200

    Fix accordion scrolls by adding unique ids to components

--- src/components/mdx/Accordion.tsx
+++ src/components/mdx/Accordion.tsx
@@ -4,30 +4,30 @@ import { twMerge } from 'tailwind-merge';
 import config from '../../../config';
 
 const AccordionContext = React.createContext({
-  activeIndex: -1,
-  setActiveIndex: (_: number): void => {},
+  active: null as string | null,
+  setActive: (_: string | null): void => {},
 });
 
 export const Accordion: React.FC<PropsWithChildren> = ({ children }) => {
-  const [activeIndex, setActiveIndex] = React.useState(-1);
+  const [active, setActive] = React.useState<string | null>(null);
   const ref = useRef(null) as React.MutableRefObject<HTMLDivElement | null>;
-  const contextValue = React.useMemo(
-    () => ({ activeIndex, setActiveIndex }),
-    [activeIndex]
-  );
+  const contextValue = React.useMemo(() => ({ active, setActive }), [active]);
 
   useEffect(() => {
-    if (activeIndex < 0 || !ref.current) return;
-    const activeTab = ref.current.querySelectorAll(
+    if (!active || !ref.current) return;
+    const tabs: NodeListOf<HTMLDivElement> = ref.current.querySelectorAll(
       '[data-rel="AccordionContent"]'
-    )[activeIndex] as HTMLDivElement | null;
-    if (!activeTab || window.scrollY < activeTab.offsetTop) return;
+    );
+    const tab: HTMLDivElement | undefined = Array.from(tabs).find(
+      (tab: HTMLDivElement) => tab.dataset.index === active
+    );
+    if (!tab || window.scrollY < tab.offsetTop) return;
     window.scrollTo({
       left: 0,
-      top: activeTab.offsetTop - config.headerOffset,
+      top: tab.offsetTop - config.headerOffset,
       behavior: 'smooth',
     });
-  }, [activeIndex]);
+  }, [active]);
 
   return (
     <AccordionContext.Provider value={contextValue}>
@@ -44,7 +44,7 @@ export const Accordion: React.FC<PropsWithChildren> = ({ children }) => {
 };
 
 interface AccordionItemProps {
-  title: string;
+  title: string | null;
   index: number;
   children: ReactNode;
 }
@@ -54,23 +54,27 @@ export const AccordionItem: React.FC<AccordionItemProps> = ({
   index,
   children,
 }) => {
-  const { activeIndex, setActiveIndex } = React.useContext(AccordionContext);
-  const hidden: boolean = index !== activeIndex;
+  const { active, setActive } = React.useContext(AccordionContext);
+  const itemId = `${title?.replace(' ', '_')}_${index}`;
+  const hidden: boolean = itemId !== active;
 
   return (
     <div
       className="w-full overflow-hidden rounded-md shadow-md"
       data-rel="AccordionContent"
+      data-index={itemId}
     >
       <button
         className="flex w-full cursor-pointer items-center justify-between overflow-hidden rounded-t-md bg-secondary-bg p-4 text-sm font-semibold focus-visible:ring-inset"
-        onClick={() => setActiveIndex(index == activeIndex ? -1 : index)}
+        onClick={() => setActive(!hidden ? null : itemId)}
       >
-        <p className="m-0">{title}</p>
+        <p className="m-0" data-rel="title">
+          {title}
+        </p>
         <span
           className={twMerge(
             'inline-flex h-3 w-3 transition-all',
-            index == activeIndex ? 'rotate-180' : 'rotate-0'
+            !hidden ? 'rotate-180' : 'rotate-0'
           )}
         >
           <IconCaretDown />
